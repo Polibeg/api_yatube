1f382d5a7be3bfa0f13f1d36d9b023b031e79536
Полина Бегалиева <POLINA.BUTTERFLY@MAIL.RU>
2023-05-17 20:01:23 +0300

    вьювсеты

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from posts.models import Group, Post, Comment
+
+from .serializers import GroupSerializer, PostSerializer, CommentSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
