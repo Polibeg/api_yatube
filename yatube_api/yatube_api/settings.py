a4ea4614f155f8a947d1477dc6c9ca4849d72e21
Полина Бегалиева <POLINA.BUTTERFLY@MAIL.RU>
2023-05-17 19:00:42 +0300

    api создание

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -23,6 +23,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api.apps.ApiConfig',
+    'rest_framework',
     'posts.apps.PostsConfig',
 ]
 
