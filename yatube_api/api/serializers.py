91aee58782ade42c2513aa968a81b88384976d18
Полина Бегалиева <POLINA.BUTTERFLY@MAIL.RU>
2023-05-18 20:42:17 +0300

    __all__ вместо перечисления полей

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -6,7 +6,7 @@ from posts.models import Group, Post, Comment
 class GroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = Group
-        fields = ('id', 'title', 'slug', 'description')
+        fields = '__all__'
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -18,14 +18,7 @@ class PostSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Post
-        fields = (
-            'id',
-            'text',
-            'author',
-            'image',
-            'group',
-            'pub_date',
-        )
+        fields = '__all__'
 
 
 class CommentSerializer(serializers.ModelSerializer):
@@ -36,5 +29,5 @@ class CommentSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Comment
-        fields = ('id', 'author', 'post', 'text', 'created',)
+        fields = '__all__'
         read_only_fields = ('post',)
